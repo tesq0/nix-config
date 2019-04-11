5ed40ae5b9ecd3fd94107339ed0c045efd6d9179
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2019-04-11 14:41:17 +0200

    update setup and readme

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Installation :
 
 1. Check the configuration files in the nixos folder and adjust them to your needs
 
-2. This will link everything in nixos/* to /etc/nixos/  `sudo ./setup.sh nix`
+2. Checkout my nixpkgs with `./setup.sh nixpkgs`
 
-3. This will download my dotfiles  `./setup.sh dotfiles`
+3. This will link everything in nixos/* to /etc/nixos/  `sudo ./setup.sh nix`
+
+4. This will download my dotfiles  `./setup.sh dotfiles`
--- setup.sh
+++ setup.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+function nix-packages() {
+		path="$HOME/Documents/Projects/nixpkgs"
+		if [ ! -e $path ] ; then
+				echo "Checking out mikus nixpkgs"
+				git clone -b mikus-stable https://github.com/tesq0/nixpkgs $path
+		else
+				echo "Already checked out local nixpkgs"
+		fi
+		for file in $(find $HOME/.nix-defexpr/*); do
+				echo "Removing $file"
+				rm $file
+		done
+
+		echo "Linking $path to $HOME/.nix-defexpr/nixpkgs"
+		ln -s $path ~/.nix-defexpr/nixpkgs
+}
+
 function nix() {
 		echo "Link nix config"
 		mkdir -p /etc/nixos
@@ -11,7 +28,7 @@ function nix() {
 }
 
 function dotfiles() {
-		if [ ! -f $HOME/.homesick/repos/homeshick ]; then
+		if [ ! -e $HOME/.homesick/repos/homeshick ]; then
 				echo "Installing homehsick"
 				git clone --depth=1 https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 		else
@@ -22,6 +39,9 @@ function dotfiles() {
 }
 
 case $1 in
+		nixpkgs)
+				nix-packages
+				;;
 		nix)
 				nix
 				;;
