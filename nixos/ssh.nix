6264105b1b651a3bd7f4f6d8b0149d7d8220a185
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2019-04-15 16:53:30 +0200

    disable x11 forwarding for now

--- nixos/ssh.nix
+++ nixos/ssh.nix
@@ -3,7 +3,12 @@
 {
     # Enable the OpenSSH daemon.
     services.openssh.enable = true;
-    services.openssh.forwardX11 = true;
     services.openssh.ports = [ 1337 ];
+    # services.openssh.forwardX11 = true;
+    # services.openssh.extraConfig = ''
+    #   AllowTcpForwarding yes
+    #   X11UseLocalhost yes
+    #   X11DisplayOffset 10
+    # '';
 
 }
