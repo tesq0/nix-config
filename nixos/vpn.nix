9a9a150f36aa5e37e264dfc289577241359913da
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2019-05-17 12:10:08 +0200

    change vpn setup

--- nixos/vpn.nix
+++ nixos/vpn.nix
@@ -1,17 +1,13 @@
 { config, pkgs, ... }:
 
 {
-
+  
   services.openvpn.servers = {
-    poland = {
-      config = builtins.readFile /media/STORAGE/vpn/my_expressvpn_poland_udp.ovpn;
+    jazajuk = {
+      config = ''config /root/nixos/openvpn/vpn-jazajuk.ovpn'';
       autoStart = true;
       updateResolvConf = true;
-      authUserPass = {
-        username = builtins.readFile /media/STORAGE/vpn/username;
-        password = builtins.readFile /media/STORAGE/vpn/password;
-      };
     };
   };
-
+  
 }
