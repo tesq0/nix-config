f63182e5a7ff0ebfc84dd726634d48fe2a7a1287
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2019-05-27 13:53:17 +0200

    Make openvpn work together with docker

--- nixos/vpn.nix
+++ nixos/vpn.nix
@@ -1,10 +1,21 @@
 { config, pkgs, ... }:
 
+let
+  routeScript = ''
+    #! /bin/sh
+    ${pkgs.iproute}/bin/ip route add default via $route_vpn_gateway
+    ${pkgs.iproute}/bin/ip route del 0.0.0.0/1 via $route_vpn_gateway
+    ${pkgs.iproute}/bin/ip route del 128.0.0.0/1 via $route_vpn_gateway
+  '';
+in
 {
   
   services.openvpn.servers = {
     jazajuk = {
-      config = ''config /root/nixos/openvpn/vpn-jazajuk.ovpn'';
+      config = ''
+        config /root/nixos/openvpn/vpn-jazajuk.ovpn
+        route-up ${pkgs.writeScript "openvpn-jazajuk-route-up" routeScript}
+      '';
       autoStart = true;
       updateResolvConf = true;
     };
