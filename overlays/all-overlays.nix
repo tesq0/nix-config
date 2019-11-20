a5a689f11a7f7679f16967f8e0e91c3641a9182c
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2019-11-20 08:51:30 +0100

    update milton package

--- overlays/all-overlays.nix
+++ overlays/all-overlays.nix
@@ -2,5 +2,8 @@ self: super:
 {
   st = super.callPackage ./pkgs/st { };
   pgmodeler = super.libsForQt5.callPackage ./pkgs/pgmodeler { };
-  milton = super.callPackage ./pkgs/milton { };
+  milton = super.callPackage ./pkgs/milton {
+    gtk2 = super.gtk2-x11;
+  };
+  pajackconnect = super.callPackage ./pkgs/pajackconnect { };
 }
--- overlays/pkgs/milton/default.nix
+++ overlays/pkgs/milton/default.nix
@@ -1,17 +1,31 @@
-{ stdenv, pkgconfig, fetchFromGitHub, cmake }:
+{ stdenv, pkgconfig, fetchFromGitHub, cmake, libGL, x11, gtk2, glib }:
 
 stdenv.mkDerivation rec {
 
   name = "milton";
 
-  src = fetchFromGitHub {
-    owner = "serge-rgb";
-    repo = "milton";
-    rev = "ff723c6c60bfcdfd59d06f867e7f8aed9b95df6a";
-    sha256 = "1xm6j7bwkr2l2ffn7hqja5nj8inc3b82x51c7xk53c00zlxggk04";
-  };
+  src = /home/mikus/Documents/Projects/milton;
+
+  # src = fetchFromGitHub {
+  #   owner = "tesq0";
+  #   repo = "milton";
+  #   rev = "ff723c6c60bfcdfd59d06f867e7f8aed9b95df6a";
+  #   sha256 = "1xm6j7bwkr2l2ffn7hqja5nj8inc3b82x51c7xk53c00zlxggk04";
+  # };
 
   nativeBuildInputs = [ pkgconfig cmake ];
+
+  cmakeFlags = [
+    "-DGTK2_DEBUG=1"
+    "-DGTK2_GDKCONFIG_INCLUDE_DIR=${gtk2}/lib/gtk-2.0/include"
+    "-DGTK2_GLIBCONFIG_INCLUDE_DIR=${glib}/lib/glib-2.0/include"
+  ];
+
+  buildInputs = [
+    libGL
+    gtk2
+    x11
+  ];
   
 }
 
