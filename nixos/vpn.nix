210b9456ea529b6baf844fc9b660d6a8d185f392
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2020-01-15 20:49:18 +0100

    auto restart vpn

--- nixos/vpn.nix
+++ nixos/vpn.nix
@@ -7,6 +7,14 @@ let
     ${pkgs.iproute}/bin/ip route del 0.0.0.0/1 via $route_vpn_gateway
     ${pkgs.iproute}/bin/ip route del 128.0.0.0/1 via $route_vpn_gateway
   '';
+  autoRestartScript = ''
+    #!/bin/sh
+    ping -c 1 -I tun0 10.8.0.1 &> /dev/null
+    code=$?
+    if [ $code -ne 0 ]; then
+    systemctl restart openvpn-jazajuk.service
+    fi
+  '';
 in
 {
   
@@ -18,6 +26,18 @@ in
       '';
       autoStart = true;
       updateResolvConf = true;
+    };
+  };
+
+  
+  systemd.services.rovpnd = {
+    description = "Auto restart openvpn service";
+    after = [ "network.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.writeScript "rovpnd" autoRestartScript}";
+      RestartSec = 6;
+      Restart = "always";
     };
   };
   
