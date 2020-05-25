3a4e03415facbf54f560a08f2f404d9c8b68c077
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2020-05-25 10:40:02 +0200

    only rsa ssh auth

--- nixos/ssh.nix
+++ nixos/ssh.nix
@@ -4,11 +4,8 @@
     # Enable the OpenSSH daemon.
     services.openssh.enable = true;
     services.openssh.ports = [ 1337 ];
-    # services.openssh.forwardX11 = true;
-    # services.openssh.extraConfig = ''
-    #   AllowTcpForwarding yes
-    #   X11UseLocalhost yes
-    #   X11DisplayOffset 10
-    # '';
+    services.openssh.extraConfig = ''
+      PasswordAuthentication no
+    '';
 
 }
