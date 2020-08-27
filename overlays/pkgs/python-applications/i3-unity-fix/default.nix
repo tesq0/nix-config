f09ecf318ccadaff5af2d64fa7c96d1b8858fb54
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2020-08-27 16:56:05 +0200

    i3-unity-fix update to v0.1.2

--- overlays/pkgs/python-applications/i3-unity-fix/default.nix
+++ overlays/pkgs/python-applications/i3-unity-fix/default.nix
@@ -3,13 +3,13 @@
 buildPythonApplication rec {
 
   pname = "i3-unity-fix";
-  version = "0.1.1";
+  version = "0.1.2";
 
   src = fetchFromGitHub {
       owner = "tesq0";
       repo = "i3-unity-fix";
       rev = "v${version}";
-      sha256 = "1f4frnxfg8l83c3aiiff1p4rqc6qy7ry7w3gcgs50mp30sinl0f2";
+      sha256 = "1kysbvvf1i6l0lhj3bvch7mccah41mwaiq7f3h8dbm8d95qwm86z";
   };
 
   propagatedBuildInputs = [ i3ipc pynput ];
