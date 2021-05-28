e95df855bdfc1f696a78bb0e663adcb577697665
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2021-05-28 08:49:23 +0200

    hp dns conf

--- nixos/configuration/hp-laptop/configuration.nix
+++ nixos/configuration/hp-laptop/configuration.nix
@@ -9,6 +9,7 @@
     [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ./environment.nix
+    ./dns.nix
     ../../base.nix
     ../../nix.nix
     ../../laptop.nix
