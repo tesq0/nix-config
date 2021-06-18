b04b08e65110b0a24ec0a33d0b145627df53d0f6
Mikolaj Galkowski <mikolaj.galkowski@gmail.com>
2021-06-18 17:54:53 +0200

    enable jack in mpv

--- overlays/all-overlays.nix
+++ overlays/all-overlays.nix
@@ -65,7 +65,7 @@ self: super:
   });
 
   pajackconnect = super.callPackage ./pkgs/pajackconnect { };
-  # mpv = super.mpv.override { jackSupport = true; };
+  mpv = super.wrapMpv (super.mpv-unwrapped.override { jackaudioSupport = true; }) {};
   vlc = super.vlc.override { jackSupport = true; };
 
   udpt = super.callPackage ./pkgs/udpt {};
