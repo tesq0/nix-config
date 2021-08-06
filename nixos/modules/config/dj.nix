1d086482f1075a577e5dc5cb2fe0d81c692701dd
Mikołaj Gałkowski <mikolaj.galkowski@vox.pl>
2021-08-06 18:50:15 +0200

    cleanup dj module

--- nixos/modules/config/dj.nix
+++ nixos/modules/config/dj.nix
@@ -9,60 +9,29 @@ in
 
   options.dj = {
     enable = mkEnableOption "DJ Kernel modifications";
+    soundcardPciId = mkOption {
+      default = "00:1f.3";
+      type = types.str;
+      description = ''
+        The PCI ID of the primary soundcard. Used to set the PCI
+        latency timer.
+
+        To find the PCI ID of your soundcard:
+            lspci | grep -i audio
+      '';
+    };
   };
 
   imports = [ ./musnix ];
 
   config = mkIf cfg.enable {
 
-
     musnix.enable = true;
-    musnix.soundcardPciId = "00:1f.3";
-    
-    # JACK
-
-    /*
-    services.jack = {
-    jackd.enable = true;
-    jackd.extraOptions = [ "-P10" "-p2048" "-dalsa" "-r48000" "-p128" "-n2" "-D" "-Chw:Device" "-Phw:CODEC" ];
-
-    # support ALSA only programs via ALSA JACK PCM plugin
-    alsa.enable = false;
-    
-    # support ALSA only programs via loopback device (supports programs like Steam)
-    loopback = {
-    enable = true;
-    # buffering parameters for dmix device to work with ALSA only semi-professional sound programs
-    dmixConfig = ''
-    period_size 2048
-    '';
-    };
-
-    };
-    */
-    
-    /*
-    systemd.services.resume-fix-pulseaudio = {
-    description = "Fix PulseAudio after resume from suspend";
-    after = [ "suspend.target" ];
-    serviceConfig = {
-    User = "mikus";
-    Type = "oneshot";
-    ExecStart = ''
-    ${pajackconnect}/bin/pajackconnect restart
-    '';
-    };
-    };
-    */
+    musnix.soundcardPciId = cfg.soundcardPciId;
     
     hardware.pulseaudio.enable = true;
     hardware.pulseaudio.support32Bit = true;
     hardware.pulseaudio.package = pkgs.pulseaudioFull;
-
-    # services.jack = {
-    #   jackd.enable = true;
-    #   alsa.enable = true;
-    # };
 
     environment.systemPackages = with pkgs ; [
       libjack2 jack2 qjackctl
@@ -70,39 +39,6 @@ in
       ardour guitarix hydrogen
     ];
 
-    # hardware.pulseaudio.extraConfig = ''
-    #   load-module module-echo-cancel
-    # '';
-
-    
-    # boot = {
-    #   kernelModules = [ "snd-seq" "snd-rawmidi" "snd-seq-midi" ];
-    #   kernel.sysctl = { "vm.swappiness" = 10; "fs.inotify.max_user_watches" = 524288; };
-    #   kernelParams = [ "threadirq" ];
-      
-    #   postBootCommands = ''
-    #     echo 2048 > /sys/class/rtc/rtc0/max_user_freq
-    #     echo 2048 > /proc/sys/dev/hpet/max-user-freq
-    #     setpci -v -d *:* latency_timer=b0
-    #     setpci -v -s $00:1f.3 latency_timer=ff
-    #   '';
-    #   # The SOUND_CARD_PCI_ID can be obtained like so:
-    #   # $ lspci ¦ grep -i audio
-    # };
-
-    # # powerManagement.cpuFreqGovernor = "performance";
-
-    # fileSystems."/" = { options = ["noatime" "errors=remount-ro"]; };
-
-    # security.pam.loginLimits = [
-    #   { domain = "@audio"; item = "memlock"; type = "-"; value = "unlimited"; }
-    #   { domain = "@audio"; item = "rtprio"; type = "-"; value = "99"; }
-    #   { domain = "@audio"; item = "nofile"; type = "soft"; value = "99999"; }
-    #   { domain = "@audio"; item = "nofile"; type = "hard"; value = "99999"; }
-    # ];
-
-
-    
   };
   
 }
