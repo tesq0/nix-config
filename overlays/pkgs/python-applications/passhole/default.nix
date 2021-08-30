a8c5479b44062dd607f954754046845486e74e42
Mikołaj Gałkowski <mikolaj.galkowski@vox.pl>
2021-08-30 12:05:39 +0200

    passhole: use remote src

--- overlays/pkgs/python-applications/passhole/default.nix
+++ overlays/pkgs/python-applications/passhole/default.nix
@@ -5,12 +5,12 @@ buildPythonApplication rec {
   pname = "passhole";
   version = "1.9.post2";
 
-  src = /home/mikus/temp/passhole;
+  # src = /home/mikus/temp/passhole;
 
-  #   fetchPypi {
-  #   inherit pname version;
-  #   sha256 = "16prj430h67w31rfzn6vk0l2h95ymzjfdhh47jq6y7h7vcr7q1hm";
-  # };
+  src = fetchPypi {
+    inherit pname version;
+    sha256 = "16prj430h67w31rfzn6vk0l2h95ymzjfdhh47jq6y7h7vcr7q1hm";
+  };
 
   propagatedBuildInputs = [ pykeepass colorama pygpgme PyUserInput xlib pykeepass-cache pynput rpyc python-daemon gnome3.zenity ];
 
